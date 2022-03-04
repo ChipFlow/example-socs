202a30c349b55aac77700090afff89e2f372d061
gatecat <gatecat@ds0.me>
2022-03-04 14:30:47 +0000

    Patch LEF capacitance too
    Signed-off-by: gatecat <gatecat@ds0.me>

--- example_socs/mpw5/scripts/extract_openrcx.tcl
+++ example_socs/mpw5/scripts/extract_openrcx.tcl
@@ -2,15 +2,18 @@ set CALIB $::env(HOME)/openrcx-calibration
 set PDK sky130B
 set BUILD_DIR build/sky130
 
-read_lef $BUILD_DIR/export/StdCellLib_res.lef
+if [info exists ::env(CORNER)] {
+	set corner $::env(CORNER)
+} else {
+	set corner max
+}
+
+read_lef -tech -library $BUILD_DIR/export/StdCellLib.patched.$corner.lef
 read_def $BUILD_DIR/export/corona_cts_export.def
 
 define_process_corner -ext_model_index 0 X
-
-foreach corner {min nom max} {
-	extract_parasitics -ext_model_file "$CALIB/openrcx-magic/$PDK/rules.openrcx.$PDK.$corner.magic" -lef_res -cc_model 12 -context_depth 10
-	write_spef $BUILD_DIR/export/corona_cts_export.$corner.spef
-}
+extract_parasitics -ext_model_file "$CALIB/openrcx-magic/$PDK/rules.openrcx.$PDK.$corner.magic" -lef_res -cc_model 12 -context_depth 10
+write_spef $BUILD_DIR/export/corona_cts_export.$corner.spef
 
 write_verilog $BUILD_DIR/export/corona_cts_export.v
 exit
--- example_socs/mpw5/scripts/splice_lef_resistance.py
+++ example_socs/mpw5/scripts/splice_lef_resistance.py
@@ -1,29 +1,64 @@
 import sys
+import os
 # Update a LEF file with layer thicknesses and resistances
 # From the original PDK
-data = {
-    "li": (0.1, 12.2),
-    "m1": (0.35, 0.125),
-    "m2": (0.35, 0.125),
-    "m3": (0.8, 0.047),
-    "m4": (0.8, 0.047),
-    "m5": (1.2, 0.047),
+
+layer_map = {
+    "li": "li1",
+    "mcon": "mcon",
+    "m1": "met1",
+    "via": "via",
+    "m2": "met2",
+    "via2": "via2",
+    "m3": "met3",
+    "via3": "via3",
+    "m4": "met4",
+    "via4": "via4",
+    "m5": "met5",
 }
+
+orig_root = f"{os.environ['HOME']}/openrcx-calibration/tech/sky130B"
 
 def main():
     curr_layer = None
-    with open(sys.argv[1], "r") as f0:
-        with open(sys.argv[2], "w") as f1:
-            for line in f0:
-                f1.write(line)
+    for corner in ("min", "nom", "max"):
+        layer_props = {}
+        curr_layer = None
+        is_port_via = False
+        with open(f"{orig_root}/sky130_fd_sc_hd.{corner}.tlef", "r") as f:
+            for line in f:
                 sl = line.strip().split(" ")
-                if sl[0] == "LAYER":
+                if len(sl) == 0:
+                    continue
+                if sl[0] == "LAYER" and not is_port_via:
                     curr_layer = sl[1]
-                elif sl[0] == "SPACING" and curr_layer is not None and curr_layer in data:
-                    th, r = data[curr_layer]
-                    print(f"   THICKNESS {th} ;", file=f1)
-                    print(f"   RESISTANCE RPERSQ {r} ;", file=f1)
+                    layer_props[curr_layer] = []
+                elif sl[0] in ("VIA", "PORT", "VIARULE"):
+                    is_port_via = True
+                elif sl[0] == "END":
+                    is_port_via = False
                     curr_layer = None
+                elif curr_layer is not None and sl[0] in ("THICKNESS", "EDGECAPACITANCE", "CAPACITANCE", "RESISTANCE"):
+                    layer_props[curr_layer].append(" ".join(sl))
+        curr_layer = None
+        is_port_via = False
+        with open(f"{sys.argv[1]}/export/StdCellLib.lef", "r") as fi:
+            with open(f"{sys.argv[1]}/export/StdCellLib.patched.{corner}.lef", "w") as fo:
+                for line in fi:
+                    sl = line.strip().split(" ")
+                    if len(sl) > 0:
+                        if sl[0] == "LAYER" and not is_port_via:
+                            curr_layer = sl[1]
+                        elif sl[0] in ("VIA", "PORT", "VIARULE"):
+                            is_port_via = True
+                        elif sl[0] == "END":
+                            is_port_via = False
+                            if curr_layer is not None:
+                                if curr_layer in layer_map:
+                                    for prop in layer_props[layer_map[curr_layer]]:
+                                        print(f"   {prop}", file=fo)
+                            curr_layer = None
+                    fo.write(line)
 
 if __name__ == '__main__':
     main()
