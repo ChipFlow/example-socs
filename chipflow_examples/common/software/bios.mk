3b213d5c7050a1135bb19a602a99e49f8c16d558
Robert Taylor <rob.taylor@chipflow.io>
2022-11-09 18:48:01 +0000

    Allow overide of check for RISCV GCC and fix tabs

--- chipflow_examples/common/software/bios.mk
+++ chipflow_examples/common/software/bios.mk
@@ -9,17 +9,18 @@ BIOS_START=generated/start.S
 DRV_SOURCES=$(wildcard $(COMMON)/drivers/*.c) $(wildcard $(COMMON)/drivers/*.S)
 DRV_HEADERS=$(wildcard $(COMMON)/drivers/*.h)
 
-
-ifeq (, $(shell which riscv32-unknown-linux-gnu-gcc))
-	ifeq (, $(shell which riscv64-unknown-elf-gcc))
-		$(error "Cannot find a riscv toolchain - please install one.")
-	else
-		RISCVGCC = riscv64-unknown-elf-gcc
-		RISCVOBJCOPY = riscv64-unknown-elf-objcopy
-	endif
-else
-	RISCVGCC = riscv32-unknown-linux-gnu-gcc
-	RISCVOBJCOPY = riscv32-unknown-linux-gnu-objcopy
+ifndef RISCVGCC
+  ifeq (, $(shell which riscv32-unknown-linux-gnu-gcc))
+    ifeq (, $(shell which riscv64-unknown-elf-gcc))
+      $(error "Cannot find a riscv toolchain - please install one.")
+    else
+      RISCVGCC = riscv64-unknown-elf-gcc
+      RISCVOBJCOPY = riscv64-unknown-elf-objcopy
+    endif
+  else
+    RISCVGCC = riscv32-unknown-linux-gnu-gcc
+    RISCVOBJCOPY = riscv32-unknown-linux-gnu-objcopy
+  endif
 endif
 
 bios.elf: $(BIOS_START) $(DRV_SOURCES) $(DRV_HEADERS) $(BIOS_HEADERS) $(BIOS_SOURCES) $(LINKER_SCR)
