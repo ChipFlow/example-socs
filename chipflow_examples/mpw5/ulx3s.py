eec5917b5febb85f0ca5c32d9a8960f51699a162
Ross Motley <ross.motley@gmail.com>
2022-11-14 11:46:16 +0000

    Don't program here as we load bitstream later in the README, and don't need to load it twice.

--- chipflow_examples/mpw5/ulx3s.py
+++ chipflow_examples/mpw5/ulx3s.py
@@ -6,4 +6,4 @@ from .soc import Mpw5SoC
 if __name__ == "__main__":
     platform = ULX3S_85F_Platform()
     # TODO: build and program BIOS, too
-    platform.build(Mpw5SoC(), do_program=True)
+    platform.build(Mpw5SoC(), do_program=False)
